c78e2dd82054281c5554a09ef105aa2854a131e6
i-Lucas <novo.contato.lucas@gmail.com>
2022-08-19 23:11:56 -0300

    styled front

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,27 +1,120 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
+import React from "react";
+import styled from "styled-components";
+import { ThreeDots } from "react-loader-spinner";
 
-function App() {
-  const [student, setStudent] = useState(null);
-    
-  useEffect(async () => {
-    const API_URL = process.env.REACT_APP_BASE_URL;
-    try {
-      const response = await axios.get(`${API_URL}/students/random`);
-      const student = response.data;
-      if(!student){
-        alert("Putz! Não há estudantes cadastrados para o sorteio!");
-      } else {
-        setStudent(student);
+export default function App() {
+
+  const API_URL = process.env.REACT_APP_BASE_URL;
+
+  const [student, setStudent] = React.useState(null);
+  const [status, setStatus] = React.useState({ message: "" });
+
+  React.useEffect(() => {
+
+    axios.get(`${API_URL}/students/random`).then(res => {
+
+      const students = res.data;
+      if (!students) {
+        return setStatus({
+          message: "No students found",
+        });
       }
-    } catch (error) {
-      alert("Não foi possível realizar o sorteio!");
-    }
-  }, []);
-  
-  return (
-    student ? <h1>{student.name}</h1> : "Carregando..."
-  )
-}
 
-export default App;
+      setStudent(students);
+
+    }).catch(err => {
+      setStatus({
+        message: "Error fetching students",
+      });
+    });
+
+  }, [API_URL, status]);
+
+  function Loader({ width, color }) {
+    return (<LoaderContainer><ThreeDots width={width} color={color} /></LoaderContainer>);
+  }
+
+  return (
+    <PageContainer>
+      {
+        status.message ?
+        
+          <Title size={"2.5rem"} bold >{status.message}</Title>
+
+          :
+
+          student ?
+
+            <ResultContainer>
+              <Title size={"2.5rem"} bold>The winner was:</Title>
+              <Title size={"1rem"} margin>{student.name}</Title>
+              <Button onClick={() => setStatus({ reload: true })}>Draw Again</Button>
+            </ResultContainer>
+
+            :
+
+            <Loader width={250} color={"white"} />
+      }
+    </PageContainer>
+  )
+};
+
+const PageContainer = styled.div`
+
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--my-color);
+`;
+
+const Title = styled.h1`
+
+  color: white;
+  font-size: ${props => props.size};
+  font-weight: ${props => props.bold ? "bold" : "normal"};
+  margin-top: ${props => props.margin ? "50px" : "0"};
+  font-family: var(--my-font);
+`;
+
+const LoaderContainer = styled.div`
+
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ResultContainer = styled.div`
+
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+
+  width: 20%;
+  height: 5%;
+  display: flex;
+  color: var(--my-color);
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+  margin-top: 50px;
+  font-weight: bold;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+  font-family: var(--my-font);
+
+  @media (max-width: 768px) {
+    width: 50%;
+  }
+`;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./assets/styles/reset.css";
+import "./assets/styles/global.css";
+
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector(".root"));
